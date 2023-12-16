4a07b0ce9585619b0cb5135ec58c2d4703162109
rdautov <dautov.rustem@gmail.com>
2023-12-16 13:23:02 +0100

    small fix

--- run.sh
+++ run.sh
@@ -2,16 +2,16 @@
 
 #for i in `seq 0 1`; do
 echo "Starting node 5000"
-python3 main.py localhost:5000 localhost:5001 localhost:5002 localhost:5003
+python3 main.py localhost:5000 localhost:5001 localhost:5002 localhost:5003 &
 
 echo "Starting node 5001"
-python3 main.py localhost:5001 localhost:5000 localhost:5002 localhost:5003
+python3 main.py localhost:5001 localhost:5000 localhost:5002 localhost:5003 &
 
 echo "Starting node 5002"
-python3 main.py localhost:5002 localhost:5000 localhost:5001 localhost:5003
+python3 main.py localhost:5002 localhost:5000 localhost:5001 localhost:5003 &
 
 echo "Starting node 5003"
-python3 main.py localhost:5003 localhost:5000 localhost:5001 localhost:5002
+python3 main.py localhost:5003 localhost:5000 localhost:5001 localhost:5002 &
 #done
 
 # This will allow you to use CTRL+C to stop all background processes
